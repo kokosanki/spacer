7a7295c6fcc2b6d71d6c0670e78d99b5aab3dc5e
kokosanki <os_mo@wp.pl>
2020-06-20 16:43:44 +0200

    Connect app to Nasa API

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
+// import axios from 'axios';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
-
+// Vue.prototype.$axios = axios;
 new Vue({
   router,
   render: (h) => h(App),
